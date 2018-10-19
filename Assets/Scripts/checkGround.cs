e9f54694ebed200acad1f4e1f21b91d90d413548
luislombardis <anguilaelectrica3@gmail.com>
2018-10-19 02:05:04 +0200

    Establecidos colliders al player y al suelo. Se ha implementado un tag de suelo y un tag de player. Se ha movido la posicion de los objetos, fondo y camara para obtener posiciones mas facilmente trabajables. Se ha organizado la carpeta assets creando una carpeta para scripts y otra para prefabs. Se han creado 3 scripts utilizados en el player. El primero es un script con los controles basicos, salto y movimiento lateral. El segundo es un script de better jump, que controla la fuerza de salto, este funciona modificando la gravedad en base al tiempo de presion de los botones. El ultimo identifica si el player esta en el suelo, para permitirle o no saltar. NOTA: Los valores dados en las variables de gravedad y salto eçstan realizadas en base a una unica plataforma situada en una posicion media-baja de la pantalla

---
+++ Assets/Scripts/checkGround.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class checkGround : MonoBehaviour {
+
+    private controls player;
+
+	// Use this for initialization
+	void Start () {
+    player = GetComponentInParent<controls>();
+	}
+
+     void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+    }
+
+     void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }   
+    }
+}
---
+++ Assets/Scripts/controls.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controls : MonoBehaviour {
+
+
+	public float maxSpeed = 5f;
+    public float speed = 2f;
+    public bool grounded;
+	public float jumPower = 10f;
+
+    
+    private Rigidbody2D rb2d;
+	private bool jump;
+
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Jump") && grounded)
+        {
+			jump = true;
+
+        }
+	}
+
+	void FixedUpdate(){
+
+		float h = Input.GetAxis("Horizontal");
+		
+		rb2d.AddForce(Vector2.right * speed * h);
+
+		float limitedSpeed = Mathf.Clamp(rb2d.velocity.x, -maxSpeed, maxSpeed);
+		rb2d.velocity = new Vector2(limitedSpeed, rb2d.velocity.y);
+
+		if (h > 0.1f){
+			transform.localScale = new Vector3(1f, 1f, 1f);
+		}
+
+		if (h < 0.1f){
+			transform.localScale = new Vector3(-1f, 1f, 1f);
+		}
+			
+		if (jump){
+
+			rb2d.AddForce(Vector2.up * jumPower, ForceMode2D.Impulse);
+			
+			jump = false;
+		}
+	}
+}
