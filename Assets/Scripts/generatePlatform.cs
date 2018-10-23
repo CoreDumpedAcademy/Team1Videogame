7cb7391010e7063ee904b96f699ee61474f00ff6
luislombardis <anguilaelectrica3@gmail.com>
2018-10-24 01:28:28 +0200

    AÃadida generacion aleatoria de plataformas
    Las plataformas se generan aleatoriamente entre 4 alturas en intervalos
    de entre 4 y 6 segundos

--- Assets/Scripts/generatePlatform.cs
+++ Assets/Scripts/generatePlatform.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class generatePlatform : MonoBehaviour {
 
-    /*private float platform = 0;
     public GameObject[] obj;
     public float tiempoMin = 4f;
     public float tiempoMax = 6f;
@@ -16,6 +15,7 @@ public class generatePlatform : MonoBehaviour {
     float y2;
     float y3;
     float y4;
+    int b;
 
     Vector3 pos;
     Vector3 pos2;
@@ -31,25 +31,46 @@ public class generatePlatform : MonoBehaviour {
 	void Update () {
 
         x = 12.4f;
-        y = Random.Range(1.5f, 3f);
+        y = Random.Range(-3f, -1.5f);
         z = 0;
-        y2 = Random.Range(0f, 1.5f);
-        y3 = Random.Range(-1.5f, 0f);
-        y4 = Random.Range(-3f, -1.5f)
+        y2 = Random.Range(-1.5f, 0f);
+        y3 = Random.Range(0f, 1.5f);
+        y4 = Random.Range(1.5f, 3f);
 
 
         pos = new Vector3(x, y, z);
+        pos2 = new Vector3(x, y2, z);
+        pos3 = new Vector3(x, y3, z);
+        pos4 = new Vector3(x, y4, z);
 
-        transform.position = pos;
 
+        int platform = Random.Range(1, 5);
+        
+        if (platform == 1)
+        {
+            transform.position = pos;
+            b = 3;
+        }
+        if (platform == 2)
+        {
+            transform.position = pos2;
+            b = 3;
+        }
+        if (platform == 3)
+        {
+            transform.position = pos3;
+            b = 4;
+        }
+        if (platform == 4)
+        {
+            transform.position = pos4;
+            b = 4;
+        }
     }
 
     void GeneradorPlataformas()
-    {   if (platform == 0)
-        {
-
-        }
+    {   
         Instantiate(obj[Random.Range(0, obj.Length)], transform.position, Quaternion.identity);
         Invoke("GeneradorPlataformas", Random.Range(tiempoMin, tiempoMax));
-    }*/
+    }
 }
