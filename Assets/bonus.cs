8fec2f1412ad64993532f971478142b8083bc46c
luislombardis <anguilaelectrica3@gmail.com>
2018-10-23 17:52:24 +0200

    Añadidas las fisicas de las plataformas
    Añadido a controls vector de velocidad al personaje cuando esta en la
    plataforma. Cambiada la velocidad del personaje.

---
+++ Assets/bonus.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bonus : MonoBehaviour {
+
+    public ScoreManager player;
+    public int puntos;
+	// Use this for initialization
+	void Start () {
+        player = GetComponentInParent<ScoreManager>();
+        puntos = 500;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Awake()
+    {
+        BoxCollider2D bc;
+        bc = gameObject.AddComponent<BoxCollider2D>() as BoxCollider2D;
+
+        bc.isTrigger = true;
+
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "1UP")
+        {
+            Destroy(col.gameObject);
+            player.score += puntos;
+            player.refreshScore();
+            Debug.Log("puntos: " + puntos);
+        }
+    }
+}
